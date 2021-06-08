3f56d1dac39553fcc6585fcfd64d7995086927ca
kijima <norimaking777@gmail.com>
2021-06-08 23:52:25 +0900

    Fix lint errors

--- src/org-to-ast.js
+++ src/org-to-ast.js
@@ -7,11 +7,7 @@ function removeUnusedProperties(node) {
   if (typeof node !== 'object') {
     return;
   }
-  ['position'].forEach((key) => {
-    if (node.hasOwnProperty(key)) {
-      delete node[key];
-    }
-  });
+  delete node.position;
 }
 
 function mapNodeType(node, parent) {
@@ -40,40 +36,32 @@ export function parse(org) {
   const tr = traverse(ast);
   tr.forEach(function (node) {
     if (this.notLeaf) {
-      // avoid conflict <input type='text' />
+      delete node.parent;
+
       // AST node has type and position
       if (node.type && node.position) {
-        // case: element => Html or ...
         node.type = mapNodeType(node, this.parent);
-      } else if (node.type === 'root') {
-        // FIXME: workaround, should fix hast
-        node.type = nodeTypes[node.type];
-        const position = src.rangeToLocation([0, org.length]);
-        // reverse adjust
-        node.position = {
-          start: {line: position.start.line, column: position.start.column + 1},
-          end: {line: position.end.line, column: position.end.column + 1}
-        };
       }
-      // Unknown type
+
       if (typeof node.type === 'undefined') {
         node.type = 'UNKNOWN';
       }
-      delete node.parent;
+
       // map `range`, `loc` and `raw` to node
       if (typeof node.position === 'object') {
-        const position = node.position;
+        const { position } = node;
         // TxtNode's line start with 1
         // TxtNode's column start with 0
         const positionCompensated = {
           start: { line: position.start.line, column: position.start.column - 1 },
-          end: { line: position.end.line, column: position.end.column - 1 }
+          end: { line: position.end.line, column: position.end.column - 1 },
         };
         const range = src.locationToRange(positionCompensated);
         node.loc = positionCompensated;
         node.range = range;
         node.raw = org.slice(range[0], range[1]);
       }
+
       // map `url` to Link node
       if (node.type === 'Link' && typeof node.properties.href !== 'undefined') {
         node.url = node.properties.href;
