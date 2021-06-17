f2ca688d60f9fb82aa3a4d895a4ebb9dfb7dc3d5
kijima <norimaking777@gmail.com>
2021-06-17 10:22:52 +0900

    Add test

--- test/ast-test.ts
+++ test/ast-test.ts
@@ -1,7 +1,20 @@
+import assert from 'power-assert';
 import { test, isTxtAST } from '@textlint/ast-tester';
 import { parse } from '../src/org-to-ast';
 
-// recommenced: test much pattern test
-const AST = parse('This is text');
-test(AST);// if the AST is invalid, then throw Error
-isTxtAST(AST);// true or false
+describe('Compliance tests', function () {
+  describe('compatible for Unist', function () {
+    it('should have position', function () {
+      const AST = parse('text');
+      test(AST); // if the AST is invalid, then throw Error
+      assert(isTxtAST(AST) === true)
+      assert(typeof AST.position === 'object');
+      assert(typeof AST.position.start === 'object');
+      assert(typeof AST.position.start.line === 'number');
+      assert(typeof AST.position.start.column === 'number');
+      assert(typeof AST.position.end === 'object');
+      assert(typeof AST.position.end.line === 'number');
+      assert(typeof AST.position.end.column === 'number');
+    });
+  });
+});
